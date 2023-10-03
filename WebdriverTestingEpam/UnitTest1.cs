995d50d217088757509890f043388543b59cfeb8
Saidmurod Mukhitdinov <saidmurodmuhitdinov@gmail.com>
2023-10-03 12:50:29 -0600

    update: variables names

--- WebdriverTestingEpam/UnitTest1.cs
+++ WebdriverTestingEpam/UnitTest1.cs
@@ -10,8 +10,8 @@ namespace WebdriverTestingEpam
     [TestClass]
     public class UnitTest1
     {
-        string test_url = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmail.google.com&ec=GAlAFw&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1678429287%3A1696101771657405&theme=glif";
-        string expected = "Inbox";
+        readonly string test_url = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmail.google.com&ec=GAlAFw&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1678429287%3A1696101771657405&theme=glif";
+        readonly string expected = "Inbox";
 
 
         [TestMethod]
@@ -24,21 +24,21 @@ namespace WebdriverTestingEpam
 
             driver.Manage().Window.Maximize();
 
-            IWebElement firstCheckBox = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            firstCheckBox.SendKeys("saidmurodtestepam@gmail.com");
+            IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+            emailLogin.SendKeys("saidmurodtestepam@gmail.com");
 
-            IWebElement zero = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
-            zero.Click();
+            IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+            nextLoginButton.Click();
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
 
-            IWebElement pass = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
-            pass.SendKeys("x@iysu27");
+            IWebElement password = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+            password.SendKeys("x@iysu27");
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-            IWebElement path = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
-            path.Click();
+            IWebElement nextPasswordButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+            nextPasswordButton.Click();
 
             IWebElement inboxActual = driver.FindElement(By.XPath("//a[text()='Inbox']"));
 
@@ -61,11 +61,11 @@ namespace WebdriverTestingEpam
             Actions actions = new Actions(driver);
 
 
-            IWebElement firstCheckBox = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            firstCheckBox.SendKeys("saidmurodtestepam@gmail.com");
+            IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+            emailLogin.SendKeys("saidmurodtestepam@gmail.com");
 
-            IWebElement zero = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
-            zero.Click();
+            IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+            nextLoginButton.Click();
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
@@ -74,22 +74,22 @@ namespace WebdriverTestingEpam
             pass.SendKeys("x@iysu27");
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-            IWebElement path = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
-            path.Click();
+            IWebElement passButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+            passButton.Click();
 
-            IWebElement round = driver.FindElement(By.CssSelector("a.gb_d.gb_Da.gb_H"));
-            round.Click();
+            IWebElement roundButton = driver.FindElement(By.CssSelector("a.gb_d.gb_Da.gb_H"));
+            roundButton.Click();
             driver.Navigate().GoToUrl("https://myaccount.google.com/?hl=en&utm_source=OGB&utm_medium=act&pli=1"); //can not find the element and click
             driver.Navigate().GoToUrl("https://myaccount.google.com/personal-info"); // can not find the element and click
 
-            IWebElement namechange = driver.FindElement(By.CssSelector("a.RlFDUe.I6g62c.N5YmOc.kJXJmd"));
-            namechange.Click();
+            IWebElement namechangePen = driver.FindElement(By.CssSelector("a.RlFDUe.I6g62c.N5YmOc.kJXJmd"));
+            namechangePen.Click();
             // could not find button
             driver.Navigate().GoToUrl("https://myaccount.google.com/profile/name/edit?continue=https://myaccount.google.com/personal-info&pli=1&rapt=AEjHL4Me6Rg3lVZv73Cqn-J7rLF2d2N_skP5XKlu48UiinQqJ2jKnYsrPEmiiDMAxnlUPhHeeNJC7zUblz6iKBOHWBn2BVxtCQ");
             
-            IWebElement namechanger = driver.FindElement(By.Id("i6"));
-            actions.DoubleClick(namechanger).Perform();
-            namechanger.SendKeys("Saidmurod");
+            IWebElement namechangerButton = driver.FindElement(By.Id("i6"));
+            actions.DoubleClick(namechangerButton).Perform();
+            namechangerButton.SendKeys("Saidmurod");
 
             IWebElement save = driver.FindElement(By.CssSelector("button.UywwFc-LgbsSe.UywwFc-LgbsSe-OWXEXe-dgl2Hf.wMI9H"));
             save.Click();
@@ -115,8 +115,8 @@ namespace WebdriverTestingEpam
             IWebElement firstCheckBox = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
             firstCheckBox.SendKeys("saidmurodtestepam@gmail.com");
 
-            IWebElement zero = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
-            zero.Click();
+            IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+            nextLoginButton.Click();
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
@@ -125,15 +125,15 @@ namespace WebdriverTestingEpam
             pass.SendKeys("x@iysu27");
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-            IWebElement path = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
-            path.Click();
+            IWebElement passButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+            passButton.Click();
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            IWebElement compose = driver.FindElement(By.XPath("//div[text()='Compose']"));
-            compose.Click();
+            IWebElement composeButton = driver.FindElement(By.XPath("//div[text()='Compose']"));
+            composeButton.Click();
 
-            IWebElement receiver = driver.FindElement(By.CssSelector("input.agP.aFw"));
-            receiver.SendKeys("saidmurod11@gmail.com");
+            IWebElement receiverEmail = driver.FindElement(By.CssSelector("input.agP.aFw"));
+            receiverEmail.SendKeys("saidmurod11@gmail.com");
 
             IWebElement emailText = driver.FindElement(By.CssSelector("div.Am.Al.editable.LW-avf.tS-tW"));
             emailText.SendKeys("Hello, Mr. Saidmurod. Hope you are doing well. Just wanted to ask you for invoice.");
@@ -147,8 +147,8 @@ namespace WebdriverTestingEpam
             var emailLogin1 = driver.FindElement(By.XPath("//input[@type='email']"));
             emailLogin1.SendKeys("saidmurod11@gmail.com");
 
-            var nextLoginButton = driver.FindElement(By.XPath("//span[text()='Next']"));
-            nextLoginButton.Click();
+            var nextLoginButton1 = driver.FindElement(By.XPath("//span[text()='Next']"));
+            nextLoginButton1.Click();
             // can not sign in
         }
     }
