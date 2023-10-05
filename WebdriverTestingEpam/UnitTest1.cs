3ab60216b5ddce20ca82f4ea37b2282432494836
Saidmurod Mukhitdinov <saidmurodmuhitdinov@gmail.com>
2023-10-05 13:02:22 -0600

    add test methods for login checker

--- WebdriverTestingEpam/UnitTest1.cs
+++ WebdriverTestingEpam/UnitTest1.cs
@@ -10,130 +10,181 @@ namespace WebdriverTestingEpam
     [TestClass]
     public class UnitTest1
     {
-        readonly string test_url = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmail.google.com&ec=GAlAFw&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1678429287%3A1696101771657405&theme=glif";
-        readonly string expected = "Inbox";
+
+            readonly string test_url = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmail.google.com&ec=GAlAFw&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1678429287%3A1696101771657405&theme=glif";
+            readonly string expected = "Inbox";
 
 
-        [TestMethod]
-        public void Login()
-        {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-
-            driver.Navigate().GoToUrl(test_url);
-
-            driver.Manage().Window.Maximize();
-
-            IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            emailLogin.SendKeys("saidmurodtestepam@gmail.com");
-
-            IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
-            nextLoginButton.Click();
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-
-            IWebElement password = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
-            password.SendKeys("x@iysu27");
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-            IWebElement nextPasswordButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
-            nextPasswordButton.Click();
-
-            IWebElement inboxActual = driver.FindElement(By.XPath("//a[text()='Inbox']"));
-
-
-            Assert.AreEqual(expected, inboxActual.Text); 
-
-            driver.Quit();
-        }
-
-
-        [TestMethod]
-        public void LoginChangeName()
-        {
-
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-
-            driver.Navigate().GoToUrl(test_url);
-
-            Actions actions = new Actions(driver);
-
-
-            IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
-            emailLogin.SendKeys("saidmurodtestepam@gmail.com");
-
-            IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
-            nextLoginButton.Click();
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-
-            IWebElement pass = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
-            pass.SendKeys("x@iysu27");
-
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
-            IWebElement passButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
-            passButton.Click();
-
-            IWebElement settingIcon = driver.FindElement(By.CssSelector("a.FH"));
-            settingIcon.Click();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-            IWebElement seeAllSettings = driver.FindElement(By.CssSelector("button.Tj"));
-            seeAllSettings.Click();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-
-            
-            string originalWindow = driver.CurrentWindowHandle;
-            IWebElement accountsAndImports = driver.FindElement(By.XPath("//a[text()='Accounts and Import']"));
-            accountsAndImports.Click();
-            IWebElement manageAccount = driver.FindElement(By.Id(":e70"));
-            manageAccount.Click();
-            wait.Until(wd => wd.WindowHandles.Count == 2);
-            foreach (string window in driver.WindowHandles)
+            [TestMethod]
+            public void Login_RightLoginPassword_SuccessfulLogin()
             {
-                if (originalWindow != window)
-                {
-                    driver.SwitchTo().Window(window);
-                    break;
-                }
+                IWebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl(test_url);
+
+                IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+                emailLogin.SendKeys("saidmurodtestepam@gmail.com");
+
+                IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+                nextLoginButton.Click();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+                IWebElement password = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+                password.SendKeys("x@iysu27");
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                IWebElement nextPasswordButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+                nextPasswordButton.Click();
+
+                IWebElement inboxActual = driver.FindElement(By.XPath("//a[text()='Inbox']"));
+
+
+                Assert.AreEqual(expected, inboxActual.Text);
+
+                driver.Quit();
             }
-            //Wait for the new tab to finish loading content
-            wait.Until(wd => wd.Title == "Gmail - Edit email address");
-
-
-            string expectedName = "Shawn Ruz";
-                        
-            IWebElement radioButton = driver.FindElement(By.Id("cfn"));
-            actions.DoubleClick(radioButton).Perform();
-            radioButton.Clear();
-            radioButton.SendKeys("Shawn Ruz");
-            string secondWindow = driver.CurrentWindowHandle;
-
-            IWebElement submitButton = driver.FindElement(By.Id("bttn_sub"));
-            submitButton.Click();
-
-            wait.Until(wd => wd.WindowHandles.Count == 1);
-            foreach (string window in driver.WindowHandles)
+            [TestMethod]
+            public void Login_RightLoginWrongPassword_StuckOnPasswordPage()
             {
-                if (secondWindow != window)
-                {
-                    driver.SwitchTo().Window(window);
-                    break;
-                }
+                IWebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl(test_url);
+
+                IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+                emailLogin.SendKeys("saidmurodtestepam@gmail.com");
+
+                IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+                nextLoginButton.Click();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+                IWebElement password = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+                password.SendKeys("x@iysu11");
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                IWebElement nextPasswordButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+                nextPasswordButton.Click();
+
+                string expectedResult = "Wrong password. Try again or click Forgot password to reset it.";
+                IWebElement actualResult = driver.FindElement(By.XPath("//span[contains(text(),'Wrong password.')]"));
+
+                Assert.AreEqual(expectedResult, actualResult.Text);
             }
-            //Wait for the new tab to finish loading content
-            wait.Until(wd => wd.Title == "Settings - saidmurodtestepam@gmail.com - Gmail");
 
-            IWebElement nameChangedChecker = driver.FindElement(By.XPath("//div[@class='rc']//following::td[@class='CY']"));
-            Assert.AreEqual(expectedName + " <saidmurodtestepam@gmail.com>", nameChangedChecker.Text);
+            [TestMethod]
+            public void Login_NoLogin_StuckOnLoginPage()
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
 
-        }
+                driver.Navigate().GoToUrl(test_url);
 
-        [TestMethod]
+                IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+                emailLogin.SendKeys("");
+
+                IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+                nextLoginButton.Click();
+
+                string expectedResult = "Enter an email or phone number";
+                IWebElement actualResult = driver.FindElement(By.XPath("//div[text()='Enter an email or phone number']"));
+
+                Assert.AreEqual(expectedResult, actualResult.Text);
+            }
+
+
+            [TestMethod]
+            public void LoginChangeName_CorrectLoginCorrectPassword_ChangedName()
+            {
+
+                IWebDriver driver = new ChromeDriver();
+                driver.Manage().Window.Maximize();
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+                driver.Navigate().GoToUrl(test_url);
+
+                Actions actions = new Actions(driver);
+
+
+                IWebElement emailLogin = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+                emailLogin.SendKeys("saidmurodtestepam@gmail.com");
+
+                IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+                nextLoginButton.Click();
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+                IWebElement pass = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+                pass.SendKeys("x@iysu27");
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+                IWebElement passButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+                passButton.Click();
+
+                IWebElement settingIcon = driver.FindElement(By.CssSelector("a.FH"));
+                settingIcon.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+                IWebElement seeAllSettings = driver.FindElement(By.CssSelector("button.Tj"));
+                seeAllSettings.Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+                string originalWindow = driver.CurrentWindowHandle;
+                IWebElement accountsAndImports = driver.FindElement(By.XPath("//a[text()='Accounts and Import']"));
+                accountsAndImports.Click();
+                IWebElement manageAccount = driver.FindElement(By.Id(":e70"));
+                manageAccount.Click();
+                wait.Until(wd => wd.WindowHandles.Count == 2);
+                foreach (string window in driver.WindowHandles)
+                {
+                    if (originalWindow != window)
+                    {
+                        driver.SwitchTo().Window(window);
+                        break;
+                    }
+                }
+                //Wait for the new tab to finish loading content
+                wait.Until(wd => wd.Title == "Gmail - Edit email address");
+
+
+                string expectedName = "Saidmurod Muhitdinov";
+
+                IWebElement radioButton = driver.FindElement(By.Id("cfn"));
+                actions.DoubleClick(radioButton).Perform();
+                radioButton.Clear();
+                radioButton.SendKeys("Saidmurod Muhitdinov");
+                string secondWindow = driver.CurrentWindowHandle;
+
+                IWebElement submitButton = driver.FindElement(By.Id("bttn_sub"));
+                submitButton.Click();
+
+                wait.Until(wd => wd.WindowHandles.Count == 1);
+                foreach (string window in driver.WindowHandles)
+                {
+                    if (secondWindow != window)
+                    {
+                        driver.SwitchTo().Window(window);
+                        break;
+                    }
+                }
+                //Wait for the new tab to finish loading content
+                wait.Until(wd => wd.Title == "Settings - saidmurodtestepam@gmail.com - Gmail");
+
+
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                IWebElement nameChangedChecker = driver.FindElement(By.XPath("//div[@class='rc']//following::td[@class='CY']"));
+                Assert.AreEqual(expectedName + " <saidmurodtestepam@gmail.com>", nameChangedChecker.Text);
+
+                driver.Quit();
+            }
+
+            [TestMethod]
 
         public void EmailSender()
         {
@@ -165,19 +216,21 @@ namespace WebdriverTestingEpam
             composeButton.Click();
 
             IWebElement receiverEmail = driver.FindElement(By.CssSelector("input.agP.aFw"));
-            receiverEmail.SendKeys("shawn@uzautotransinc.com");
+            receiverEmail.SendKeys("saidmurodmum@mail.ru");
 
             IWebElement emailText = driver.FindElement(By.CssSelector("div.Am.Al.editable.LW-avf.tS-tW"));
-            emailText.SendKeys("Hi Shawn!");
+            emailText.SendKeys("Hi saidmurod, hope you are ok. Do not forget to finish your task.");
 
             IWebElement sendButton = driver.FindElement(By.XPath("//div[text()='Send']"));
             sendButton.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
-            string differentAccountUrl = "https://accounts.google.com/v3/signin/identifier?authuser=0&continue=https%3A%2F%2Fmail.google.com&ec=GAlAFw&hl=en&service=mail&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S-1274899217%3A1696355119845412&theme=glif";
+
+            string differentAccountUrl = "https://mail.ru/";
             driver.Navigate().GoToUrl(differentAccountUrl);
 
-            var emailLogin1 = driver.FindElement(By.XPath("//input[@type='email']"));
-            emailLogin1.SendKeys("saidmurod11@gmail.com");
+            var emailLogin1 = driver.FindElement(By.CssSelector("button.ph-login.svelte-1ke9xx5"));
+            emailLogin1.Click();
 
             var nextLoginButton1 = driver.FindElement(By.XPath("//span[text()='Next']"));
             nextLoginButton1.Click();
