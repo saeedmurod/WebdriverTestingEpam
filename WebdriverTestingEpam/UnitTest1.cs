e6cecc384640e844e3911073b90a6e5b0d7030f5
Saidmurod Mukhitdinov <saidmurodmuhitdinov@gmail.com>
2023-10-06 13:04:59 -0600

    add assert to email method

--- WebdriverTestingEpam/UnitTest1.cs
+++ WebdriverTestingEpam/UnitTest1.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using System.Threading.Tasks;
 
 namespace WebdriverTestingEpam
 {
@@ -185,6 +186,104 @@ namespace WebdriverTestingEpam
 
         [TestMethod]
 
+        public void EmailSender_LoginSendEmailReadReply_SuccessfulReply()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl(test_url);
+
+            driver.Manage().Window.Maximize();
+
+            IWebElement firstCheckBox = driver.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+            firstCheckBox.SendKeys("saidmurodtestepam@gmail.com");
+
+            IWebElement nextLoginButton = driver.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+            nextLoginButton.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+            IWebElement pass = driver.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+            pass.SendKeys("x@iysu27");
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            IWebElement passButton = driver.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+            passButton.Click();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            IWebElement composeButton = driver.FindElement(By.XPath("//div[text()='Compose']"));
+            composeButton.Click();
+
+            IWebElement receiverEmail = driver.FindElement(By.CssSelector("input.agP.aFw"));
+            receiverEmail.SendKeys("steve@uzautotransinc.com");
+
+            IWebElement emailText = driver.FindElement(By.CssSelector("div.Am.Al.editable.LW-avf.tS-tW"));
+            emailText.SendKeys("Hi saidmurod, hope you are ok. Do not forget to finish your task.");
+
+            try
+            {
+                IWebElement sendButton = driver.FindElement(By.XPath("//div[text()='Send']"));
+                sendButton.Click();
+            }
+            catch (OpenQA.Selenium.UnhandledAlertException)
+            {
+                driver.SwitchTo().Alert().Accept();
+            }
+
+            Thread.Sleep(10000);
+            driver.Quit();
+
+
+            IWebDriver driver1 = new ChromeDriver();
+
+            driver1.Manage().Window.Maximize();
+
+            driver1.Navigate().GoToUrl(test_url);
+
+
+            IWebElement firstCheckBox1 = driver1.FindElement(By.XPath("//*[@id=\"identifierId\"]"));
+            firstCheckBox1.SendKeys("steve@uzautotransinc.com");
+
+
+            IWebElement nextLoginButton1 = driver1.FindElement(By.XPath("//*[@id=\"identifierNext\"]/div/button/span"));
+            nextLoginButton1.Click();
+
+            driver1.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+            IWebElement pass1 = driver1.FindElement(By.XPath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+            pass1.SendKeys("xzsaiu77");
+
+
+            IWebElement passButton1 = driver1.FindElement(By.XPath("//*[@id=\"passwordNext\"]/div/button/span"));
+            passButton1.Click();
+
+            Thread.Sleep(3000);
+            IWebElement notRead = driver1.FindElement(By.XPath("//span[text()='Hi saidmurod, hope you are ok. Do not forget to finish your task.']"));
+            notRead.Click();
+
+            IWebElement reply = driver1.FindElement(By.CssSelector("span.ams.bkH"));
+            reply.Click();
+
+            Thread.Sleep(3000);
+            IWebElement textboxReply = driver1.FindElement(By.CssSelector("div.Am.aO9.Al.editable.LW-avf.tS-tW"));
+            string textToMatch = "Hello, thank you for your message. Will work on your proposal";
+            textboxReply.SendKeys("Hello, thank you for your message. Will work on your proposal");
+
+            IWebElement replyButton = driver1.FindElement(By.CssSelector("div.T-I.J-J5-Ji.aoO.v7.T-I-atl.L3"));
+            replyButton.Click();
+            Thread.Sleep(3000);
+
+            IWebElement expectedText = driver1.FindElement(By.XPath("//div[text()='Hello, thank you for your message. Will work on your proposal']"));
+
+            Assert.AreEqual(textToMatch, expectedText.Text);
+
+            driver.Quit();
+        }
+
+        [TestMethod]
+
         public void EmailSender()
         {
             IWebDriver driver = new ChromeDriver();
@@ -266,7 +365,8 @@ namespace WebdriverTestingEpam
             reply.Click();
 
             Thread.Sleep(3000);
-            IWebElement textboxReply = driver1.FindElement(By.Id(":u9"));
+            IWebElement textboxReply = driver1.FindElement(By.CssSelector("div.Am.aO9.Al.editable.LW-avf.tS-tW"));
+            string textToMatch = "Hello, thank you for your message. Will work on your proposal";
             textboxReply.SendKeys("Hello, thank you for your message. Will work on your proposal");
 
             IWebElement replyButton = driver1.FindElement(By.CssSelector("div.T-I.J-J5-Ji.aoO.v7.T-I-atl.L3"));
